dddd775b5fd401fe3fcc4e8279b9a5f92b206985
Developer <comps381f@ouhk.edu.hk>
2017-11-21 01:04:44 +0800

    first commit

--- server.js
+++ server.js
@@ -10,36 +10,6 @@ var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var mongourl = 'mongodb://home:123321@ds141454.mlab.com:41454/t03';
 
-function read_n_print(res) {
-	MongoClient.connect(mongourl, function(err, db) {
-		assert.equal(err,null);
-		console.log('Connected to MongoDB\n');
-		findUserPw(db,function(users) {
-			db.close();
-			console.log('Disconnected MongoDB\n');
-			if (users.length == 0) {
-				console.log('Not found!');
-			} else {
-				console.log(users);
-			}
-		}); 
-	});
-}
-
-function findUserPw(db,callback) {
-	var users = [];
-	cursor = db.collection('accounts').find();
-
-	cursor.each(function(err, doc) {
-		assert.equal(err, null); 
-		if (doc != null) {
-			users.push(doc);
-		} else {
-			callback(users); 
-		}
-	});
-}
-
 app = express();
 app.set('view engine','ejs');
 
@@ -61,21 +31,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.get('/',function(req,res) {
+app.get('/',function(req,res,next) {
 	console.log(req.session);
 	if (!req.session.authenticated) {
-		res.redirect('/login');
-	} else {
 		res.status(200);
-		res.render('secrets',{userid:req.session.username});
+		res.render('login');
+	} else {
+		res.redirect('/read');
 	}
 });
 
-app.get('/login',function(req,res) {
-	res.sendFile(__dirname + '/public/login.html');
+app.get('/login',function(req,res,next) {
+	res.redirect('/');
+	//res.status(200);
+	//res.render('login');
+	//res.sendFile(__dirname + '/public/login.html');
 });
 
-app.post('/login',function(req,res) {
+app.post('/login',function(req,res,next) {
+	//req.session = null;
 	MongoClient.connect(mongourl, function(err, db) {
 		assert.equal(err,null);
 		console.log('Connected to MongoDB\n');
@@ -84,6 +58,7 @@ app.post('/login',function(req,res) {
 			console.log('Disconnected MongoDB\n');
 			if (users.length == 0) {
 				console.log('Not found!');
+				res.redirect('/');
 			} else {
 				//console.log(users);
 				for (var i=0; i<users.length; i++) {
@@ -99,9 +74,41 @@ app.post('/login',function(req,res) {
 	});
 });
 
-app.post('/logout',function(req,res) {
+app.get('/logout',function(req,res,next) {
 	req.session = null;
 	res.redirect('/');
 });
+
+app.get('/read',function(req,res,next) {
+	if (!req.session.authenticated) {
+		res.redirect('/');
+		return next();
+	}
+	res.status(200);
+	res.render('read',{userid:req.session.username});
+});
+
+app.get('/new',function(req,res,next){
+	if (!req.session.authenticated) {
+		res.redirect('/');
+		return next();
+	}
+	res.status(200);
+	res.render('new');
+});
+
+function findUserPw(db,callback) {
+	var users = [];
+	cursor = db.collection('accounts').find();
+
+	cursor.each(function(err, doc) {
+		assert.equal(err, null); 
+		if (doc != null) {
+			users.push(doc);
+		} else {
+			callback(users); 
+		}
+	});
+}
 
 app.listen(process.env.PORT || 8099);
